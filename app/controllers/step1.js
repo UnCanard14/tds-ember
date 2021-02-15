be6c50cefac0fe47715b0c23f4a94b33b46764ea
Simon <sim.debray@laposte.net>
2021-02-15 18:55:54 +0100

    la liste ne s'affiche pas

---
+++ app/controllers/step1.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class Step1Controller extends Controller {
+  dispoItems = [];
+  includedItems = [];
+  @tracked disposItems_ = [];
+  @tracked includedItems_ = [];
+
+  changeDispo(elements){
+
+  }
+
+  changeIncluded(elements){
+
+  }
+
+  add(source, dest, what){
+
+  }
+
+  remove(source, dest, what){
+
+  }
+}
