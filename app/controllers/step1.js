e5193bf3d8e9239e9b75435154c0a36a3cd8e1e9
Simon <sim.debray@laposte.net>
2021-02-17 13:14:54 +0100

    some modifications

--- app/controllers/step1.js
+++ app/controllers/step1.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class Step1Controller extends Controller {
   dispoItems = [];
@@ -8,15 +9,21 @@ export default class Step1Controller extends Controller {
   @tracked includedItems_ = [];
 
   changeDispo(elements){
-
+    //undefined variable i dont know why ?
+    //this.includedItems.pushObject(elements);
   }
 
+  @action
   changeIncluded(elements){
-
+    this.includedItems_ = elements;
   }
 
+  @action
   add(source, dest, what){
-
+    console.log(what);
+    debugger;
+    dest.pushObjects(what);
+    source.removeObjects(what);
   }
 
   remove(source, dest, what){
