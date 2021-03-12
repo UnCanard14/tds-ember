c27ab90afd7ea20df67f16f14bbaac1b9d4c8c27
Simon <sim.debray@laposte.net>
2021-03-12 16:24:53 +0100

    add auth

---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+import Route from '@ember/routing/route';
+import action from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class IndexRoute extends Route {
+  @service userAuth;
+
+  @action connexion(email, password) {
+    this.store
+      .query('employee', {
+        filter: {
+          email: email,
+        },
+      })
+      .then((users) => {
+        if (users.length) {
+          let user = users.firstObject;
+          if (password == user.password) {
+            this.userAuth.login(user);
+            //this.transition;
+          }
+        }
+      });
+  }
+}
