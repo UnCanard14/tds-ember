1ead116bf52c1c1538094c77dd858f3bd9be86f0
Simon <sim.debray@laposte.net>
2021-03-17 18:57:37 +0100

    some modification

--- app/routes/board.js
+++ app/routes/board.js
@@ -1,8 +1,11 @@
 import Route from '@ember/routing/route';
-import AbstractrouteRoute from "./abstractroute";
+import AbstractrouteRoute from './abstractroute';
 
 export default class BoardRoute extends AbstractrouteRoute {
-  model(){
+  model() {
     return this.userAuth.getUser();
+ //   return this.store
+ //     .findAll('odrder')
+ //     .filterBy('idUser', this.userAuth.getUser());
   }
 }
